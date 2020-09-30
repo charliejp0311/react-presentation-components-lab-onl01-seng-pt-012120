1c475712f606cfe72b33ed4b9a40d03d84d6b7ee
Charlie Pelton <charliejp0311@gmail.com>
2020-09-30 13:35:42 -0500

    Done.

--- .history/src/components/SimplerComponent_20200930133113.js
+++ .history/src/components/SimplerComponent_20200930133113.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const SimplerComponent = (props) => {
     // debugger
-    return <div onClick={props.handleClick}>I am just happy</div>
+    return <div handleClick={props.handleClick}>I am just happy</div>
 }
 
 export default SimplerComponent;
